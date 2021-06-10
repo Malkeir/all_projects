ef970d5489ab96ebc0a909e729c2cea3ca20b4d5
malkeir <badorr@charter.net>
2021-06-10 19:23:52 -0400

    start files for all classes

---
+++ pawn.py
@@ -0,0 +1,23 @@
+
+
+
+class pawn:
+
+    health = 0
+    maxHealth = 0
+    ac = 0
+    movementSpeed = 0
+    stre,dex,con,wis,intel,cha = 0
+    numAttacks = 1
+    numBonusActions = 1
+    numActions = 1
+    numReactions = 1
+    lvl = 0
+
+    def init(self):
+
+
+
+
+    def modify(self,statchoice):
+        (self.statechoice-10)/2
